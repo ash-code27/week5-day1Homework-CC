e230b30b8df566d031582d4462267c0268f51dc6
ash-code27 <ashley_hinton17@yahoo.com>
2021-04-15 04:32:05 -0500

    first commit

---
+++ car_collection/authentication/routes.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from car_collection.forms import UserLoginForm
+from car_collection.models import User,db
+
+auth = Blueprint('auth', __name__, template_folder='auth_templates')
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    form = UserLoginForm()
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            print(email, password)
+
+            user = User(email, password = password)
+            
+            db.session.add(user)
+            db.session.commit()
+
+            flash(f'You have successfully created a user account {email}', 'user_created')
+
+            return redirect(url_for('site.home'))
+    except:
+        raise Exception('Invalid Form Data: Please Check Your Form Inputs')
+
+    return render_template('signup.html', form = form)
+
+@auth.route('/signin', methods = ['GET', 'POST'])
+def signin():
+    return render_template('signin.html')
